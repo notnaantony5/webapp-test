c26be78040786b287c623af263cc1d03dba55b09
Vitek <lol@mail.com>
2025-08-19 19:10:22 +0300

    update

--- database.py
+++ database.py
@@ -1,14 +1,24 @@
 from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
 from datetime import datetime
 
-engine = create_engine("sqlite3://db.sqlite3")
+engine = create_engine("sqlite:///db.sqlite3")
 session_maker = sessionmaker(engine)
 
-class PostModel(DeclarativeBase):
+class Base(DeclarativeBase):
+    ...
+
+class PostModel(Base):
     __tablename__ = "posts"
 
-    id: Mapped[int]
-    title: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50), unique=True)
     content: Mapped[str]
     created_at: Mapped[datetime]
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_tables()
